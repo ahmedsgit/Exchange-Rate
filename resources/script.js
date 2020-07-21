f7b5f29e9188f4ff35e7b189eaa107984cba20db
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-21 20:10:38 +0600

    Done Design Part

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,5 @@
-let first_cu
+let first_currencyEle = document.getElementById('first_currency');
+let second_currencyEle = document.getElementById('second_currency');
+let first_amountEle = document.getElementById('first-amount');
+let second_amountEle = document.getElementById('second-amount');
+let rate = document.getElementById('rate')
