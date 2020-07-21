050721c5d59af8cddbf86f8efa5dc6d2e5bbe1fc
ahmedsgit <acc.ahmedsazzad@gmail.com>
2020-07-21 21:06:16 +0600

    Add JS

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,35 @@
-let first_currencyEle = document.getElementById('first_currency');
-let second_currencyEle = document.getElementById('second_currency');
-let first_amountEle = document.getElementById('first-amount');
-let second_amountEle = document.getElementById('second-amount');
-let rate = document.getElementById('rate')
+let firstCurrencyEle = document.getElementById('first-currency');
+let firstAmountEle = document.getElementById('first-amount');
+let secondCurrencyEle = document.getElementById('second-currency');
+let secondAmountEle = document.getElementById('second-amount');
+
+let rateEle = document.getElementById('rate');
+let changeBtn = document.getElementById('change');
+
+//Fetch exchange rates and update the DOM.
+function calculate() {
+    const firstCurrency = firstCurrencyEle.value;
+    const secondCurrency = secondCurrencyEle.value;
+    //fetch data
+    fetch(`https://api.exchangerate-api.com/v4/latest/${firstCurrency}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.rates[secondCurrency];
+            rateEle.innerText = `1 ${firstCurrency} = ${rate} ${secondCurrency}`;
+            secondAmountEle.value = (firstAmountEle.value * rate).toFixed(2)
+        })
+}
+
+//Event listeners
+firstCurrencyEle.addEventListener('change', calculate);
+firstAmountEle.addEventListener('input', calculate);
+secondCurrencyEle.addEventListener('change', calculate);
+secondAmountEle.addEventListener('input', calculate);
+
+changeBtn.addEventListener('click', () => {
+    const temp = firstCurrencyEle.value;
+    firstCurrencyEle.value = secondCurrencyEle.value;
+    secondCurrencyEle.value = temp;
+    firstAmountEle.value = secondAmountEle.value;
+    calculate();
+})
